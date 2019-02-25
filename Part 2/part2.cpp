e57d868db5b80354877f7c892cf87b01b125a125
midori <pigsdofly6@gmail.com>
2019-02-25 15:55:23 +0000

    Got iterative version of rect rule working

--- Part 2/part2.cpp
+++ Part 2/part2.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
+#include "part2.h"
+
 int main() {
-    typedef ADD<EXP<VAR, LIT<2>, <SUB<MULT<LIT<2>,VAR>, LIT<3>>> POLY;
+    typedef ADD<EXP<VAR, LIT<2>>, SUB< MULT< LIT<2>, VAR>, LIT<3>>> POLY;
     POLY p;
-    
+    typedef INTEGRATE<0, 10, RECT<POLY>> INTEGRATION;
+    INTEGRATION i;
+
+    std::cout << p.eval(3) << std::endl;
+
+    std::cout << "10/200 is: " << 10.0 / 200.0 << std::endl;
+    std::cout << "Integrating polynomial over the interval [0,10] with 200 divisions: " << i.integrate(200.0) << std::endl;
+
     return 0;
 }
--- Part 2/part2.h
+++ Part 2/part2.h
@@ -1,17 +1,46 @@
 #ifndef __part2_h__
 #define __part2_h__
 #include "part1.h"
+#include <iostream>
 
 // integrate function - lower bound, upper bound and divisions. 
 // interpolation function 
 // trapezoidal rule and rectangle rule
 // loops must be statically unrolled
-template <class P, int LOWER, int HIGHER, class RULE>
-struct INTEGRATE {
-    static inline double integrate(int divisions) {
-        
+//iterative way
+
+template <class P>
+struct RECT {
+    static inline double eval(double div, int LOWER, int HIGHER) {
+        double sum = 0;
+        P p;
+        //return RECTUNROLL<LOWER>::();
+        for (double i = LOWER; i <= HIGHER; i += div) {
+            double midpoint = (i + (div + i))/2;
+            sum += div * p.eval(midpoint);
+        }
+        return sum;
+    }
+};
+/*
+template <double LOWER>
+struct RECTUNROLL {
+    static inline double eval(double div, P p) {
+        double midpoint = (LOWER + (div + LOWER))/2;
+        return (div * p.eval(midpoint)) + RECTUNROLL<LOWER+div>::eval(div, p);
     };
 };
+*/
+template <class P>
+struct TRAP {};
 
+template <int LOWER, int HIGHER, class RULE>
+struct INTEGRATE {
+    static inline double integrate(int divisions) {
+        double div = ((double) HIGHER - (double) LOWER) / divisions;
+        
+        return RULE::eval(div, LOWER, HIGHER);
+    };
+};
 
 #endif
